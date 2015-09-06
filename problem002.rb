a18b906ed6ed6953dd74f9f69816c1c4d849afd0
Arek Jaskólski <arek.jaskolski@gmail.com>
2015-09-06 10:53:32 +0200

    Solved problem number 2

---
+++ problem002.rb
@@ -0,0 +1,16 @@
+def fibonacci( n )
+  return  n  if ( 0..1 ).include? n
+  fibonacci(n - 1) + fibonacci(n - 2)
+end
+
+i = 0
+items = []
+
+loop do
+    fib = fibonacci(i)
+    break if fib >= 4_000_000
+    items << fib if fib.even?
+    i += 1
+end
+
+p items.inject(:+)
